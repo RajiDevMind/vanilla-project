8a3658d56e97e0a0caca652f2c75be66ecb69d8e
Raji DevMind <omoiyaalasso@gmail.com>
2023-09-04 04:30:08 -0700

    image slider funtionality was implemented

--- react-project/src/pages/ContentPage.jsx
+++ react-project/src/pages/ContentPage.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import bulb from "../assets/bulb-iot.jpg";
-import iootz2 from "../assets/ioootz-2.jpg";
+import React, { useState } from "react";
 import iootz3 from "../assets/ioootz-3.jpg";
 import iootz1 from "../assets/vr-iootz.jpg";
 
 const ContentPage = () => {
+  const [prev, setPrev] = useState(false);
+  const [next, setNext] = useState(false);
+
+  let updateImg = () => {
+    if (currentSlide > next.length) {
+      return (currentSlide = 1);
+    } else if (currentSlide < 1) {
+      currentSlide = prev.length;
+    }
+  };
+
+  let currentSlide = 1;
+  const handleClick = () => {
+    setNext(!next) || setPrev(!prev);
+    updateImg();
+  };
+
+  updateImg();
+
   return (
     <>
       <div className="page">
@@ -47,15 +64,28 @@ const ContentPage = () => {
             </span>
           </p>
         </div>
-        <div className="wrapper">
-          <div>
-            <img src={iootz3} alt="" />
-          </div>
-          <div>
-            <img src={iootz1} alt="" />
-          </div>
-          <div>
-            <img src={iootz2} alt="" />
+        <div className="container">
+          <div className="wrapper">
+            <div className="image-slider">
+              <div
+                className={`icon-container ${
+                  next ?? prev ? "transformed" : ""
+                }`}
+              >
+                <img className="imgslider" src={iootz3} alt="image" />
+
+                <img className="imgslider" src={iootz1} alt="image" />
+              </div>
+              <i
+                className="fa-solid fa-angles-left btn prev"
+                onClick={handleClick}
+              ></i>
+
+              <i
+                className="fa-solid fa-angles-right btn next"
+                onClick={handleClick}
+              ></i>
+            </div>
           </div>
         </div>
       </div>
